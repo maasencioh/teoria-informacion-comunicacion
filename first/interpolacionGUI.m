69010093baa1065c010e7a4ca939f30aedbe47a8
anmrdz <anmrdz@gmail.com>
2016-04-04 03:57:33 -0500

    agregado script e imagenes interpolacion

--- first/interpolacionGUI.m
+++ first/interpolacionGUI.m
@@ -6,9 +6,9 @@ f = figure('Visible','off','Position',[360,500,450,285]);
 htext = uicontrol('Style','text','String','Frecuencia de muestreo',...
     'Position',[315,220,70,25]);
 hinputtext = uicontrol('Style','edit','Position',[315,180,70,25]);
-hgraph = uicontrol('Style','pushbutton','String','Graficar interpolacion lineal',...
+hgraph = uicontrol('Style','pushbutton','String','Int. lineal',...
     'Position',[315,140,70,25], 'Callback',@graphbutton_Callback);
-hgraphsp = uicontrol('Style','pushbutton','String','Graficar interpolacion pchip',...
+hgraphsp = uicontrol('Style','pushbutton','String','Int. P. Lagrange',...
     'Position',[315,100,70,25], 'Callback',@graphspbutton_Callback);
 ha = axes('Units','Pixels','Position',[50,60,200,185]);
 align([htext,hinputtext,hgraph, hgraphsp],'Center','None');
@@ -48,20 +48,40 @@ set(f,'Visible','on');
         nmax = floor(tmax / T);
         n = nmin:nmax;
         vq1 = interp1(t,ya, n*T);
+        hold on
         plot(t,ya,n*T,vq1,':.');
         grid
+        hold off
     end
 
     function graphspbutton_Callback(source,eventdata)
-        x = str2num(get(hinputtext, 'string'))
-        Fs = x*frec;
+        xf = str2num(get(hinputtext, 'string'))
+        Fs = xf*frec;
         T = 1/Fs;
         nmin = ceil(tmin / T);
         nmax = floor(tmax / T);
-        n = nmin:nmax;
-        vq1 = pchip(t,ya, n*T);
-        plot(t,ya,n*T,vq1,'*');
+        xi = 0:0.01:2;
+        N = nmax-nmin
+        x = linspace(tmin, tmax, N);
+        y = sin(2*pi*frec * x);
+        
+        n = length(x) - 1;
+        ni = length(xi);
+        L = ones(ni,n+1); 
+        
+        for j = 1 : (n+1)
+            for i = 1 : (n+1)
+                if (i ~= j)
+                    L(:,j) = L(:,j).*(xi' - x(i))/(x(j)-x(i));
+                end
+            end
+        end
+        
+        yi = y*L';
+        
+        plot(t,ya,xi,yi,'r.');
         grid
+        
     end
 
 end
